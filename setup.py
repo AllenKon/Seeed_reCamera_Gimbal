65004cfbee4f6426ba42c812d960357bd6886737
AllenKon <125844485+AllenKon@users.noreply.github.com>
2024-07-16 15:18:22 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="reCamera_Gimbal",
+    version="1.0",
+    packages=find_packages(),
+    install_requires=[
+        
+    ],
+    entry_points={
+        'console_scripts': [
+            
+        ],
+    },
+    author="Allen",
+    author_email="akong1999@163.com",
+    description="reCamera Gimbal Programmable Development Kit",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url="https://github.com/AllenKon/Seeed_reCamera_Gimbal/",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
